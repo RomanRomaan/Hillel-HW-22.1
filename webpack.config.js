535c6ab9c71da1a58aa113f8dd9cd131a07126f6
RomanRomaan <jay4or@gmail.com>
2025-08-21 17:52:49 +0300

    2

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,52 @@
+// webpack.config.js
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
-    entry: './src/script.js',               // main entry script.js
+    entry: './src/script.js',
     output: {
-        filename: 'bundle.js',                // final js
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true
+        clean: true,
+        assetModuleFilename: 'assets/[name][ext]'
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,    // css to new file
-                    'css-loader'
-                ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|webp)$/i,
+                type: 'asset/resource'
             }
         ]
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: 'styles.css' }) // finale CSS
+        new MiniCssExtractPlugin({ filename: 'styles.css' }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',   // 👈 указываем правильный путь
+            inject: 'body'
+        })
     ],
     optimization: {
         minimize: true,
         minimizer: [
-            new TerserPlugin({ extractComments: false }), // minifik JS
-            new CssMinimizerPlugin()                      // minifik CSS
+            new TerserPlugin({ extractComments: false }),
+            new CssMinimizerPlugin()
         ],
         concatenateModules: true
+    },
+    devServer: {
+        static: { directory: path.resolve(__dirname, 'dist') }, // теперь сервер будет брать index.html из dist
+        port: 8080,
+        open: true,
+        hot: false,
+        client: { overlay: true }
     },
     devtool: false
 };
