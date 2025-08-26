4802c52ab96160da78caa4ba922c3d91072906ba
RomanRomaan <jay4or@gmail.com>
2025-08-26 12:46:35 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         })
     ],
     optimization: {
-        minimize: true,
+        minimize: false,
         minimizer: [new TerserPlugin({ extractComments: false }), new CssMinimizerPlugin()],
         concatenateModules: true
     },
